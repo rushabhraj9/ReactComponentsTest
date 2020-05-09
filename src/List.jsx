efd635202c635f85b836e7439c685bd901ba26f1
rushabhraj9 <40721290+rushabhraj9@users.noreply.github.com>
2020-05-09 19:37:51 +0530

    Added2Components
    Added List and Adding Components

---
+++ src/List.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function List() {
+  return (
+    <ul>
+      <li>Hello</li>
+      <li>Kaise Ho</li>
+      <li>Namaskar Mitron</li>
+    </ul>
+  );
+}
+export { List };
--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const date=new Date();
-const hours=date.getHours();
-let wish="";
-let color="";
-if(hours>=24|| hours<=12){
-   wish="Good Morning !";
-   color="red";
-}
-if(hours>12 && hours<18){
-  wish="Good Afternoon !";
-  color="green";
-}
-if(hours>=18 && hours<24){
-  wish="Good Evening !";
-  color="blue";
-}
+import { List } from "./List";
+import Heading from "./Heading.jsx";
+const date = new Date();
+
+const hours = date.getHours();
+
 ReactDOM.render(
   <div>
-    <h1 style={{color:color,size:"150px"}}>{wish}</h1>
+    <Heading />
+    <List />
   </div>,
-  document.getElementById("root"));
- 
+  document.getElementById("root")
+);
